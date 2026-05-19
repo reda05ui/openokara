ab25d38519d7267311adeb1afb1c3876f4544a1f
reda05ui <mhd.reda05@gmail.com>
2026-05-20 02:02:13 +0300

    Update next.config.ts

--- web/next.config.ts
+++ web/next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["127.0.0.1"],
   output: "standalone",
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:9000/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
